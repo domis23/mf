4a96b2be5c89a4f122e7646c1b37bfed6f9bdaa6
domis23 <96925584+domis23@users.noreply.github.com>
2024-10-18 17:17:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".go").addEventListener('click', () => {
+    var firstname = document.querySelector(".firstname");
+    var surname = document.querySelector(".surname");
+    var image = document.querySelector(".image");
+
+   
+});
